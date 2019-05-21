6106086a35dee3314aa41713190732ed4f2bbd36
Sindre Sorhus <sindresorhus@gmail.com>
2019-05-21 16:44:18 +0700

    Revert "Drop duplicate export (#84)"
    This reverts commit 28913cae88026e785658256c0d89eaf3c8101fd6.
    
    Closes #89
    Fixes #88

--- source/index.ts
+++ source/index.ts
@@ -385,5 +385,8 @@ Object.defineProperties(is, {
 	}
 });
 
-module.exports = is; // For CommonJS default export support
 export default is;
+
+// For CommonJS default export support
+module.exports = is;
+module.exports.default = is;
